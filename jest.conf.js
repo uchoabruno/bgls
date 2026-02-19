19c4755236268140304b1a8590a2a63bcda00bae
Bruno Uchoa <bruno@metafactory.nl>
2026-02-19 15:21:10 +0100

    Configuring sonar jest

--- jest.conf.js
+++ jest.conf.js
@@ -6,7 +6,7 @@ const {
 const environment = require('./webpack/environment');
 
 module.exports = {
-  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$|dayjs/esm)'],
+  transformIgnorePatterns: ['node_modules/(?!.*.mjs$|dayjs/esm)'],
   resolver: 'jest-preset-angular/build/resolvers/ng-jest-resolver.js',
   globals: {
     ...environment,
@@ -14,13 +14,17 @@ module.exports = {
   roots: ['<rootDir>', `<rootDir>/${baseUrl}`],
   modulePaths: [`<rootDir>/${baseUrl}`],
   setupFiles: ['jest-date-mock'],
-  cacheDirectory: '<rootDir>/target/jest-cache',
-  coverageDirectory: '<rootDir>/target/test-results/',
+  cacheDirectory: '<rootDir>/target/jest-cache', // Este também pode precisar de ajuste, se <rootDir> não for a raiz do Maven
+  coverageDirectory: '<rootDir>/target/test-results/', // Este também pode precisar de ajuste
   moduleNameMapper: pathsToModuleNameMapper(paths, { prefix: `<rootDir>/${baseUrl}/` }),
   reporters: [
     'default',
-    ['jest-junit', { outputDirectory: '<rootDir>/target/test-results/', outputName: 'TESTS-results-jest.xml' }],
-    ['jest-sonar', { outputDirectory: './target/test-results/jest', outputName: 'TESTS-results-sonar.xml' }],
+    // Opcional: Se você ainda precisa do relatório jest-junit para outros fins,
+    // ajuste o caminho para que ele também vá para o target raiz.
+    // ['jest-junit', { outputDirectory: '../../target/test-results/', outputName: 'TESTS-results-jest.xml' }],
+
+    // Este é o principal que precisamos ajustar para o SonarQube
+    ['jest-sonar', { outputDirectory: '../../target/test-results/jest', outputName: 'TESTS-results-sonar.xml' }],
   ],
   testMatch: ['<rootDir>/src/main/webapp/app/**/@(*.)@(spec.ts)'],
   testEnvironmentOptions: {
